1fabf48721ccc8d53759b53b96e2bc1416d1a4cc
MinyeongHonggit config --global user.email agka00161@gmail.comcd C:/Users/user/Desktop/Git <78523467+MinyeongHong@users.noreply.github.com>
2022-04-01 19:41:52 +0900

    uni check

--- lib/app/ui/page/signup/screen/univ_check_screen.dart
+++ lib/app/ui/page/signup/screen/univ_check_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
@@ -7,10 +9,11 @@ import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:uni_meet/app/ui/page/post/post_screen.dart';
+import 'package:uni_meet/app/ui/page/signup/screen/confetti_screen.dart';
 import '../../../../../secret/secret_keys.dart';
 import '../../../../controller/auth_controller.dart';
 import '../../../../data/model/app_user.dart';
-
 
 class UnivCheckScreen extends StatefulWidget {
   const UnivCheckScreen({Key? key}) : super(key: key);
@@ -20,28 +23,24 @@ class UnivCheckScreen extends StatefulWidget {
 }
 
 class _UnivCheckScreenState extends State<UnivCheckScreen> {
-
   File? imageFile;
   String parsedtext = '';
-  String name = Get.arguments.name; // 파이어베이스 연동 후 고쳐야 함
+  String name = Get.arguments.name;
   String uni = Get.arguments.university;
-
- // String grade = "18학번";
+  // String grade = Get.arguments.age + "학번";
   String everytime = "학교 인증";
   bool flag1 = false, flag2 = false, flag3 = false;
+  bool? uni_check;
 
-  Future _getFromGallery() async{
-    final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+  Future _getFromGallery() async {
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
     if (pickedFile == null) return;
-    final image_temporary= File(pickedFile.path);
-    if (pickedFile == null) {
-      print('fail');
-      return;
-    }
-    else print("사진 픽 성공");
+    final image_temporary = File(pickedFile.path);
 
     setState(() {
       this.imageFile = image_temporary;
+      _Recognition(imageFile);
     });
   }
 
@@ -50,14 +49,18 @@ class _UnivCheckScreenState extends State<UnivCheckScreen> {
     String img64 = base64Encode(bytes);
 
     var url = 'https://api.ocr.space/parse/image';
-    var payload = {"base64Image": "data:image/jpg;base64,${img64.toString()}","language" :"kor"};
-    var header = {"apikey" :auth_image_key};
+    var payload = {
+      "base64Image": "data:image/jpg;base64,${img64.toString()}",
+      "language": "kor"
+    };
+    var header = {"apikey": auth_image_key};
 
-    var post = await http.post(Uri.parse(url),body: payload,headers: header);
+    var post = await http.post(Uri.parse(url), body: payload, headers: header);
     var result = jsonDecode(post.body);
 
     setState(() {
       parsedtext = result['ParsedResults'][0]['ParsedText'];
+      uni_check = txtCheck();
     });
   }
 
@@ -65,9 +68,9 @@ class _UnivCheckScreenState extends State<UnivCheckScreen> {
     flag1 = parsedtext.contains(name);
     flag2 = parsedtext.contains(uni);
     flag3 = parsedtext.contains(everytime);
-//     if(flag1) print("이름 확인");
-//     if(flag2) print("학교 확인");
-//     if(flag3) print("에타 확인");
+    if (flag1) print("이름 확인");
+    if (flag2) print("학교 확인");
+    if (flag3) print("에타 확인");
 
     if (flag1 && flag2 && flag3)
       return true;
@@ -101,8 +104,8 @@ class _UnivCheckScreenState extends State<UnivCheckScreen> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           BackButton(),
-          Text(uni),
-          Text(name),
+          Text("임시 텍스트" + uni),
+          Text("임시 텍스트" + name),
           Text(
             "먼저 학교 인증을 해야해요",
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
@@ -112,49 +115,123 @@ class _UnivCheckScreenState extends State<UnivCheckScreen> {
           Spacer(
             flex: 1,
           ),
-          Container(
-              height: MediaQuery.of(context).size.width * 0.85,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(color: Colors.grey),
-              child: Center(
-                child: imageFile == null
-                    ? Image.asset("assets/images/uniexample.png")
-                    : Image.file(imageFile!),
-
-              )),
+          Stack(
+            children: [
+              Container(
+                  height: MediaQuery.of(context).size.width * 0.85,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(color: Colors.grey),
+                  child: Center(
+                    child: imageFile == null
+                        ? Image.asset("assets/images/uniexample.png")
+                        : Image.file(imageFile!),
+                  )),
+              Positioned.fill(
+                child: Align(
+                  alignment: Alignment.center,
+                  child: ElevatedButton(
+                      style: ButtonStyle(
+                        elevation: MaterialStateProperty.all(10),
+                      ),
+                      onPressed: () {
+                        _getFromGallery();
+                      },
+                      child: Text(
+                        "사진 업로드",
+                      )),
+                ),
+              ),
+            ],
+          ),
           Spacer(
             flex: 1,
           ),
-          ElevatedButton(
-              onPressed: () {
-                _getFromGallery();
-
-              },
-              child: Text("사진 업로드")
-          ),
-          ElevatedButton(
-              onPressed: () {
-
-                if (txtCheck())
-                  {
+          Center(
+            child: ElevatedButton(
+                onPressed: () {
+                  if (imageFile == null) {
                     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
                       content: const Text(
-                        "인증이 완료되었습니다.",
+                        "이미지를 업로드 해 주세요 !",
                         style: TextStyle(color: Colors.white),
                       ),
-                      backgroundColor: Colors.black,
-                    ));
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      content: const Text(
-                        "인증에 실패하였습니다. 다시 시도해 주세요.",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      backgroundColor: Colors.black,
+                      backgroundColor: Colors.blue,
                     ));
                   }
-              },
-              child: Text("인증하기")),
+
+                  else {
+                    if(uni_check==null){
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: const Text(
+                          "인식이 진행중입니다. 잠시 후 시도해주세요!",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        backgroundColor: Colors.black,
+                      ));
+                    }
+                    else if (uni_check==true) {
+                      showDialog(
+                          context: context,
+                          //barrierDismissible - Dialog를 제외한 다른 화면 터치 x
+                          barrierDismissible: false,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              // RoundedRectangleBorder - Dialog 화면 모서리 둥글게 조절
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0)),
+                              //Dialog Main Title
+                              title: Column(
+                                children: <Widget>[
+                                  new Text("Dialog Title"),
+                                ],
+                              ),
+                              //
+                              content: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(
+                                    "인증이 완료되었습니다.",
+                                  ),
+                                ],
+                              ),
+                              actions: <Widget>[
+                                new TextButton(
+                                  child: new Text("확인"),
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                    Get.to(PostScreen());
+                                  },
+                                ),
+                              ],
+                            );
+                          });
+                      uni_check=null;
+                    }
+                    else {
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        content: const Text(
+                          "인증에 실패하였습니다. 다시 시도해 주세요.",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        backgroundColor: Colors.black,
+                      ));
+                      uni_check=null;
+                    }
+                  }
+                },
+                child: Text("인증하기"),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.resolveWith((states) {
+                    if (imageFile != null) {
+                      return Colors.blue;
+                    } else {
+                      return Colors.grey[500];
+                    }
+                  },
+                  ),
+                )),
+          ),
           Spacer(
             flex: 2,
           ),
@@ -172,15 +249,18 @@ class _UnivCheckScreenState extends State<UnivCheckScreen> {
                         barrierDismissible: false,
                         builder: (BuildContext child_context) {
                           return AlertDialog(
-                            content: Text("사진 업로드 버튼을 통해 스크린에 캡쳐 파일을 띄운 후, 전송하기를 누르면 24시간 내로 학교 인증이 완료됩니다."),
+                            content: Text(
+                                "사진 업로드 버튼을 통해 스크린에 캡쳐 파일을 띄운 후, 전송하기를 누르면 24시간 내로 학교 인증이 완료됩니다."),
                             actions: [
                               Center(
                                 child: Column(
                                   children: [
                                     ElevatedButton(
                                         onPressed: () {
-                                          if(imageFile != null) uploadFB(imageFile!);}, child: Text("전송하기")),
-
+                                          if (imageFile != null)
+                                            uploadFB(imageFile!);
+                                        },
+                                        child: Text("전송하기")),
                                   ],
                                 ),
                               )
@@ -198,4 +278,6 @@ class _UnivCheckScreenState extends State<UnivCheckScreen> {
       ),
     )));
   }
+
+
 }
